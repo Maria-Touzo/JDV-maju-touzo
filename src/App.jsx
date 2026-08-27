cb79dc9e32e870718d579d7c0d4b966eaf85ddf3
Maria-Touzo <maria.touzo@aluno.senai.br>
2026-08-27 08:59:48 -0300

    add: analisando as funções e tratando erros.

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,14 @@
-import { useState } from 'react';
+
 import Game from './game';
-import Square from './Square'
 
-
-
-function App() {
-
-  // const objeto = 
-  // {
-  //   id: 1, 
-  //   nome:'João'
-  // };
-
-  // const styles = 
-  // {
-  //   containerFrutas: 'classe-azul',
-  //   'container-legumes': 'classe-verde'
-  // };
-
-  return (
+function App(){
+  return(
     <>
-
-    {/* <Alunos nome={'jeni'} idade={17} ativo={true}/>
-    <Alunos nome={'jeni'} idade={17} ativo={true}/>
-    <Alunos nome={'jeni'} idade={17} ativo={true}/>
-
-    <Botao texto={"Clique aqui!"}/>
-
-    <Card/>
-
-    <p>Ponto: {objeto.nome}</p>
-    <p>Colchetes: {objeto['nome']}</p>
-
-    <MsgUsuario nomeUsuario={'SENAI'}/>
-    <MsgUsuario autenticado={true} nomeUsuario={'SENAI'}/>
-
-    <UseStateHook/> */}
-     {/* <Contador/>  */}
-    {/* <UseEffectHook/> */}
-     {/* <Tema /> 
-      */}
-    {/* <ConsultaApi/> */}
-      {/* <ConsultaApiSa/> */}
-      {/* <Produtos/> */}
-      <Consulta/>
+    <Game/>
     </>
-  );
-};
+    
+  )
+
+}
 
 export default App
